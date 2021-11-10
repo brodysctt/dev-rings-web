2036fa97beccc3f1a4ec137ec5ea32d96d6cda9c
bscott4 <brodyscott12+dev@gmail.com>
2021-11-10 11:44:11 -0800

    writing events to firestore 🔥 🔥 🔥 🔥

--- functions/src/github/receiveWebhookEvent.ts
+++ functions/src/github/receiveWebhookEvent.ts
@@ -1,4 +1,6 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import { db } from "../config";
 
 import * as cors from "cors";
 // @ts-ignore
@@ -9,23 +11,86 @@ export const receiveWebhookEventHandler = functions.https.onRequest(
     corsHandler(req, res, async () => {
       try {
         const eventType = req.header("X-GitHub-Event");
-        const data = req.body;
+        if (eventType !== "push" && eventType !== "pull_request") {
+          functions.logger.log(`${eventType} events are currently not handled`);
+          res.sendStatus(200);
+          return;
+        }
+
+        const { body: data } = req;
+        const {
+          sender: { login },
+        } = data;
+        // TODO: update this to be dynamic
+        // ^ i.e. add github username to the User document when user first completes auth
+        if (login !== "bscott4") {
+          functions.logger.log(
+            `Event created by ${login} – this event won't be stored`
+          );
+          res.sendStatus(200);
+          return;
+        }
+
+        const ref = db
+          .collection("users")
+          .doc("brodyscott12+dev@gmail.com")
+          .collection("events");
 
         if (eventType === "push") {
-          const { commits } = data;
-          functions.logger.log(`just pushed ${commits.length} commits`);
+          const {
+            commits,
+            repository: { name: repo },
+          } = data;
+
+          if (!commits.length) {
+            functions.logger.log(
+              `Push event for creating or deleting a branch – this event won't be stored.`
+            );
+            res.sendStatus(200);
+            return;
+          }
+
+          for (const commit of commits) {
+            const { id: commitId, message, url: url } = commit;
+            await ref.doc(commitId).set({
+              createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+              message,
+              repo,
+              type: eventType,
+              url,
+            });
+          }
           res.sendStatus(200);
           return;
         }
 
         if (eventType === "pull_request") {
-          functions.logger.log(`just put up a bigggg PR`);
+          functions.logger.log(`officially within the pull_request block`);
+          const { action } = data;
+          if (action !== "opened") {
+            functions.logger.log(
+              `PR event for ${action} action – this event won't be stored`
+            );
+            res.sendStatus(200);
+            return;
+          }
+
+          const {
+            pull_request: { id: pullRequestId, url, title: message },
+            repository: { name: repo },
+          } = data;
+
+          await ref.doc(pullRequestId).set({
+            createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+            message,
+            repo,
+            type: eventType,
+            url,
+          });
+
           res.sendStatus(200);
           return;
         }
-
-        functions.logger.log(`${eventType} events are currently not handled`);
-        res.sendStatus(200);
       } catch (err) {
         res.status(400).send(err);
       }
