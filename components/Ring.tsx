e78e7bcaa804c531664bc11f7b845ab419becf02
bscott4 <brodyscott12+dev@gmail.com>
2021-11-16 13:52:59 -0800

    okay, i see u Ring.tsx 👀 👑

--- components/Ring.tsx
+++ components/Ring.tsx
@@ -1,19 +1,25 @@
 import * as React from "react";
-export const Ring = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg className="prefix__progress-ring" height={250} width={250} {...props}>
-    <linearGradient id="prefix__a" x1="0%" y1="0%" x2="0%" y2="100%">
-      <stop offset="0%" stopColor="#7E55D6" />
-      <stop offset="100%" stopColor="#D655AD" />
-    </linearGradient>
-    <circle
-      className="prefix__progress-ring__circle"
-      strokeWidth={32}
-      strokeLinecap="round"
-      stroke="url(#prefix__a)"
-      fill="transparent"
-      r={109}
-      cx={125}
-      cy={125}
-    />
-  </svg>
-);
+
+export const RADIUS = 109;
+export const Ring = ({ offset }: { offset: number }) => {
+  const center = 125;
+  const circumference = 2 * Math.PI * RADIUS;
+  return (
+    <svg height={250} width={250} fill="#2A2A30">
+      <linearGradient id="gradient" x1="100%" y1="100%" x2="70%" y2="40%">
+        <stop offset="0%" stopColor="#CAD2F7" />
+        <stop offset="100%" stopColor="#556cd6" />
+      </linearGradient>
+      <circle
+        strokeWidth={32}
+        strokeLinecap="round"
+        stroke="url(#gradient)"
+        r={RADIUS}
+        cx={center}
+        cy={center}
+        strokeDasharray={circumference}
+        strokeDashoffset={offset}
+      />
+    </svg>
+  );
+};
--- pages/dev-rings.tsx
+++ pages/dev-rings.tsx
@@ -1,22 +1,78 @@
 import type { NextPage } from "next";
-import { Box } from "@mui/material";
-import { Ring } from "components";
+import { Box, Button } from "@mui/material";
+import { Ring, RADIUS } from "components";
+import { useState, useEffect } from "react";
 
 const DevRings: NextPage = () => {
+  const [progress, setProgress] = useState(20);
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    const circumference = 2 * Math.PI * RADIUS;
+    const progressOffset = ((100 - progress) / 100) * circumference;
+    setOffset(progressOffset);
+  }, [progress, offset]);
+
+  const incrementProgress = () => {
+    console.log("Incrementing progress...");
+    setProgress(progress + 5);
+  };
+
+  const decrementProgress = () => {
+    console.log("Decrementing progress...");
+    setProgress(progress - 5);
+  };
+
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "80vh",
-        width: "100%",
-      }}
-    >
-      <Ring />
+    <Box sx={containerSx}>
+      <Box sx={ringContainerSx}>
+        <Box sx={dotSx} />
+        <Ring offset={offset} />
+      </Box>
+      <Box sx={buttonContainerSx}>
+        <Button
+          variant="contained"
+          onClick={() => incrementProgress()}
+          sx={{ mb: "20px" }}
+        >
+          increase
+        </Button>
+        <Button variant="contained" onClick={() => decrementProgress()}>
+          decrease
+        </Button>
+      </Box>
     </Box>
   );
 };
 
 export default DevRings;
+
+const containerSx = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+} as const;
+
+const ringContainerSx = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "60vh",
+} as const;
+
+const dotSx = {
+  position: "absolute",
+  borderRadius: "50%",
+  height: "120px",
+  width: "120px",
+  bgcolor: "white",
+} as const;
+
+const buttonContainerSx = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  ml: "40px",
+} as const;
