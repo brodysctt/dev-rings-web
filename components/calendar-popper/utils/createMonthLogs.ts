9c2c547787b23b8dec785c7eb3456bcbf402e259
bscott4 <brodyscott12+dev@gmail.com>
2021-12-02 16:02:01 -0800

    calendar now recognizes end of month days off 🏖️

--- components/calendar-popper/utils/createMonthLogs.ts
+++ components/calendar-popper/utils/createMonthLogs.ts
@@ -5,16 +5,19 @@ export const createMonthLogs = (
   hasPrevious: boolean,
   monthInView: MonthYear
 ) => {
+  const [month, year] = monthInView;
+
   const dayLogs: ZeroIndexDayLog[] = logs.map((log) => {
     const [dateString, rest] = log;
     return [new Date(dateString).getDate() - 1, rest];
   });
-  const [[firstDay]] = dayLogs;
+  const [[firstLoggedDay]] = dayLogs;
+  const [[lastLoggedDay]] = dayLogs.slice(-1);
 
   let i = 0;
   let dayOffIndexes = [];
   for (const dayLog of dayLogs) {
-    const dayCount = !hasPrevious ? i + firstDay : i;
+    const dayCount = !hasPrevious ? i + firstLoggedDay : i;
     const [day] = dayLog;
     const daysOff: number = day - dayCount;
     if (daysOff > 0) {
@@ -26,11 +29,20 @@ export const createMonthLogs = (
     i++;
   }
 
+  const lastDayOfMonth = new Date(year, month, 0).getDate() - 1; // zero-indexed
+  const endOfMonthDaysOff = lastDayOfMonth - lastLoggedDay;
+
+  if (endOfMonthDaysOff > 0) {
+    for (let i = 0; i < endOfMonthDaysOff; i++) {
+      dayOffIndexes.push(lastDayOfMonth + i);
+    }
+    i++;
+  }
+
   dayOffIndexes.forEach((dayOffIndex) => {
     dayLogs.splice(dayOffIndex, 0, [dayOffIndex, { actual: 0, goal: 0 }]);
   });
 
-  const [month, year] = monthInView;
   const completeLogs: Log[] = dayLogs.map((dayLog) => {
     const [zeroIndexDay, rest] = dayLog;
     const day = zeroIndexDay + 1;
