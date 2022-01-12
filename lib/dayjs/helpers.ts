df876aa62b190be5c0f215319f52952a32de33b6
bscott4 <brodyscott12+dev@gmail.com>
2022-01-11 20:36:58 -0800

    whoops

--- lib/dayjs/helpers.ts
+++ lib/dayjs/helpers.ts
@@ -1,8 +1,27 @@
 import type { Dayjs } from "dayjs";
 import { dayjs } from "@lib/dayjs";
+import type { RepoEvent } from "@lib/firebase/firestore";
 
 export const checkTimezone = (tz: string) =>
   dayjs().utcOffset() !== dayjs().tz(tz).utcOffset();
+
+export const getDayEvents = (
+  events: RepoEvent[] | null,
+  dateString: string
+) => {
+  if (!events) return null;
+  const dateStringEvents = events.map((event) => [
+    dayjs(event.createdAt.toDate()).format("YYYY-MM-DD"),
+    event,
+  ]);
+
+  const dayEvents = dateStringEvents
+    .filter((event) => event[0] === dateString)
+    .map((event) => event[1]) as RepoEvent[];
+
+  if (!dayEvents.length) return null;
+  return dayEvents;
+};
 
 export const getMonthName = (monthInView: MonthYear) => {
   const [month] = monthInView;
