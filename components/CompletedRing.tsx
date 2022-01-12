56c40b6c73f75946b10c539fd9de3e9d7e20520e
bscott4 <brodyscott12+dev@gmail.com>
2022-01-12 13:28:11 -0800

    CompletedRing now uses variables for dimensions

--- components/CompletedRing.tsx
+++ components/CompletedRing.tsx
@@ -1,61 +1,73 @@
 import { motion } from "framer-motion";
 import { Box } from "@mui/material";
 
-export const CompletedRing = () => (
-  <Box
-    sx={{
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
-    <motion.svg
-      width="300"
-      height="300"
-      viewBox="0 0 300 300"
-      initial="hidden"
-      animate="visible"
+interface Props {
+  isMini?: boolean;
+}
+
+export const CompletedRing = ({ isMini = false }: Props) => {
+  const size = isMini ? 20 : 300;
+  const ringStrokeWidth = isMini ? 4 : 20;
+  const checkStrokeWidth = isMini ? 4 : 15;
+  const checkStart = [130, 155];
+  const checkVertex = isMini ? [115, 150] : [145, 170];
+  const checkEnd = [180, 135];
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
     >
-      <motion.circle
-        cx="150"
-        cy="150"
-        r="100"
-        stroke="#556cd6"
-        strokeWidth={20}
-        strokeLinecap="round"
-        fill="transparent"
-        style={{ rotate: 270 }}
-        variants={draw}
-        custom={1}
-      />
-      <motion.line
-        x1="130"
-        y1="155"
-        x2={VERTEX[0]}
-        y2={VERTEX[1]}
-        stroke={CHECKMARK_COLOUR}
-        strokeWidth={15}
-        strokeLinecap="round"
-        custom={3}
-        variants={draw}
-      />
-      <motion.line
-        x1={VERTEX[0]}
-        y1={VERTEX[1]}
-        x2="180"
-        y2="135"
-        stroke={CHECKMARK_COLOUR}
-        strokeWidth={15}
-        strokeLinecap="round"
-        custom={4.2}
-        variants={draw}
-      />
-    </motion.svg>
-  </Box>
-);
+      <motion.svg
+        width={size}
+        height={size}
+        viewBox={`0 0 ${size} ${size}`}
+        initial="hidden"
+        animate="visible"
+      >
+        <motion.circle
+          cx={size / 2} // check its 150
+          cy={size / 2} // check its 150
+          r={size / 3} // check its 100
+          stroke="#556cd6"
+          strokeWidth={ringStrokeWidth}
+          strokeLinecap="round"
+          fill="transparent"
+          style={{ rotate: 270 }}
+          variants={draw}
+          custom={1}
+        />
+        <motion.line
+          x1={checkStart[0]}
+          y1={checkStart[1]}
+          x2={checkVertex[0]}
+          y2={checkVertex[1]}
+          stroke={CHECKMARK_COLOUR}
+          strokeWidth={checkStrokeWidth}
+          strokeLinecap="round"
+          custom={3}
+          variants={draw}
+        />
+        <motion.line
+          x1={checkVertex[0]}
+          y1={checkVertex[1]}
+          x2={checkEnd[0]}
+          y2={checkEnd[1]}
+          stroke={CHECKMARK_COLOUR}
+          strokeWidth={checkStrokeWidth}
+          strokeLinecap="round"
+          custom={4.2}
+          variants={draw}
+        />
+      </motion.svg>
+    </Box>
+  );
+};
 
 const CHECKMARK_COLOUR = "#66CC00";
-const VERTEX = [145, 170];
 
 const draw = {
   hidden: { pathLength: 0, opacity: 0 },
