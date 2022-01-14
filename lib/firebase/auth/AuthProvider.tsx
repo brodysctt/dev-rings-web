7ac53534216f47693f7f0d798032631c84a6d378
bscott4 <brodyscott12+dev@gmail.com>
2022-01-14 15:02:33 -0800

    auth tweak 🔧

--- lib/firebase/auth/AuthProvider.tsx
+++ lib/firebase/auth/AuthProvider.tsx
@@ -24,7 +24,7 @@ export const AuthProvider = ({ children }: any) => {
       setUser(user);
       Cookies.set("token", token);
     });
-  }, [router]);
+  }, [user, router]);
 
   // TODO: Why do I want to force refresh the token every 10 minutes?
   useEffect(() => {
