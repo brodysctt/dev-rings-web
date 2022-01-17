9b3fc2ad716109ec8e4381616cb5376ded907124
bscott4 <brodyscott12+dev@gmail.com>
2022-01-17 08:08:01 -0800

    🔧

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -14,7 +14,7 @@ const Enter = () => {
 
   if (!userId || !userData)
     return (
-      <Stack height="100vh">
+      <Stack justifyContent="center" alignItems="center" height="100vh">
         <ProgressRing values={[1, 1]} />
         <Typography variant="h4" sx={{ mt: 3, mb: 5, color: "primary.main" }}>
           {`Build momentum on your coding journey`}
