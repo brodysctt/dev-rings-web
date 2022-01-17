d0cde2d9acb0d1486cf907064d5092bfc04c60f5
bscott4 <brodyscott12+dev@gmail.com>
2022-01-16 19:09:39 -0800

    regex fix 🔧

--- functions/src/middleware.ts
+++ functions/src/middleware.ts
@@ -1,5 +1,5 @@
 import cors from "cors";
 
 export const corsMiddleware = cors({
-  origin: "/https://dev-rings-web.*bscott4.vercel.app/",
+  origin: new RegExp("/https://dev-rings-web.*bscott4.vercel.app/"),
 });
