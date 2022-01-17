878a1c128664131b943cf37fb25d0d83af543e9f
bscott4 <brodyscott12+dev@gmail.com>
2022-01-17 12:01:05 -0800

    first pass at responsive calendar

--- components/calendar-popper/DayTile.tsx
+++ components/calendar-popper/DayTile.tsx
@@ -4,6 +4,7 @@ import { useAuth } from "@lib/firebase/auth";
 import type { Log } from "@lib/firebase/firestore";
 import { dayjs } from "@lib/dayjs";
 import { AnimatedRing, ProgressRing } from "components";
+import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import ButtonBase from "@mui/material/ButtonBase";
 import type { SxProps } from "@mui/system";
@@ -24,33 +25,32 @@ export const DayTile = ({ log }: { log: Log }) => {
   };
   return (
     <Link href={isToday ? "/" : dayHref} passHref>
-      <ButtonBase
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-        disabled={isDayOff}
-        sx={{ ...buttonSx, bgcolor: hover ? "#F5F6FD" : null }}
+      <Stack
+        border="3px solid #DCDEE6"
+        borderRadius={3}
+        maxHeight={60}
+        maxWidth={60}
+        pt={0.5}
       >
-        <Typography sx={{ fontSize: 10, alignSelf: "flex-end", mr: "4px" }}>
+        <Typography mr={1} sx={{ fontSize: 10, alignSelf: "flex-end" }}>
           {dayjs(dateString).date()}
         </Typography>
-        {hitGoal ? (
-          <AnimatedRing size={40} isDayTile />
-        ) : (
-          <ProgressRing values={isDayOff ? [0, 1] : [actual, goal]} size={30} />
-        )}
-      </ButtonBase>
+        <ButtonBase
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          disabled={isDayOff}
+          sx={{ bgcolor: hover ? "#F5F6FD" : null }}
+        >
+          {hitGoal ? (
+            <AnimatedRing size={40} isDayTile />
+          ) : (
+            <ProgressRing
+              values={isDayOff ? [0, 1] : [actual, goal]}
+              size={30}
+            />
+          )}
+        </ButtonBase>
+      </Stack>
     </Link>
   );
 };
-
-const buttonSx = {
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "flex-start",
-  alignItems: "center",
-  height: 60,
-  width: 60,
-  border: "3px solid #DCDEE6",
-  borderRadius: 3,
-  pt: "2px",
-} as SxProps;
