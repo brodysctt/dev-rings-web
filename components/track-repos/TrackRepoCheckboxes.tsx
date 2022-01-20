08faa298a5b2f3acaba6be50901658558b8029a9
bscott4 <brodyscott12+dev@gmail.com>
2022-01-20 01:23:32 -0800

    SubmitButton for TrackRepoCheckboxes 👌

--- components/track-repos/TrackRepoCheckboxes.tsx
+++ components/track-repos/TrackRepoCheckboxes.tsx
@@ -1,11 +1,15 @@
 import { useState, ChangeEvent, useEffect } from "react";
 import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import { useAuth } from "@lib/firebase/auth";
 import { trackRepo } from "components/track-repos/trackRepo";
 import { useRepos } from "components/track-repos/hooks";
+import { toast } from "react-toastify";
+import Lottie from "react-lottie-player";
+import loadingDotsJson from "public/loading-dots.json";
 
 export const TrackRepoCheckboxes = () => {
   const [untrackedRepos] = useRepos();
@@ -80,15 +84,28 @@ interface Props {
 }
 
 export const SubmitButton = ({ repos, disabled }: Props) => {
+  const [isLoading, setIsLoading] = useState(false);
   const userId = useAuth();
   if (!userId) return null;
 
   const handleClick = () => {
     if (disabled) return;
+    setIsLoading(true);
     repos.forEach(async (repo) => await trackRepo(userId, repo as string));
+    setIsLoading(false);
+    toast.success(`Successfully tracked ${repos.length} repos`);
   };
 
-  return (
+  return isLoading ? (
+    <Box
+      height={83}
+      width={200}
+      mt={-6}
+      sx={{ position: "relative", zIndex: -1 }}
+    >
+      <Lottie loop animationData={loadingDotsJson} play />
+    </Box>
+  ) : (
     <Button variant="contained" disabled={disabled} onClick={handleClick}>
       {`Track repos`}
     </Button>
