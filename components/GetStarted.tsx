081b53956ed5f2eab9f338451d652636ba947cf0
bscott4 <brodyscott12+dev@gmail.com>
2022-01-27 11:25:38 -0800

    added female avatar 🎉

--- components/GetStarted.tsx
+++ components/GetStarted.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
-import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
 import Lottie from "react-lottie-player";
 // @ts-ignore
 import zenDeveloperJson from "https://assets9.lottiefiles.com/packages/lf20_9e8yoqkm.json";
+import zenFemaleDeveloperJson from "public/avatar-female.json";
 
 export const GetStarted = () => (
   <Stack alignItems="center">
@@ -17,10 +17,13 @@ export const GetStarted = () => (
         <Image src="/blobhighfive.png" width={30} height={30} />
       </Stack>
     </Stack>
-    <Tooltip title="More inclusive animations coming soon">
+    <Stack direction="row">
       <Box height={500} width={500}>
         <Lottie loop animationData={zenDeveloperJson} play speed={0.7} />
       </Box>
-    </Tooltip>
+      <Box height={500} width={500}>
+        <Lottie loop animationData={zenFemaleDeveloperJson} play speed={0.7} />
+      </Box>
+    </Stack>
   </Stack>
 );
